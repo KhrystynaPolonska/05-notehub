e187a51116dafa4d7ccd7942628615500bc97c73
kristy <krisposol7@gmail.com>
2025-10-23 21:03:49 +0200

    1

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
-import type {  NoteCreate, Note, NoteResponse } from "../types/note";
+import type { Note, NoteCreate } from "../types/note";
+import type { NoteResponse } from "../types/note";
 
-const BASE_URL = "https://notehub-public.goit.study/api/notes"
+const BASE_URL = "https://notehub-public.goit.study/api/notes";
 
-export async function fetchNotes (page = 1, perPage = 12, search='') {
-  
+export async function fetchNotes(page = 1, perPage = 12, search = '') {
   const params: Record<string, string | number> = {
     page,
     perPage,
@@ -13,33 +13,32 @@ export async function fetchNotes (page = 1, perPage = 12, search='') {
   if (search.trim()) {
     params.search = search.trim();
   }
-  console.log("Request params:", params);
+
   const response = await axios.get<NoteResponse>(BASE_URL, {
     params,
     headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_NTHB_TOKEN}`,
+      Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`,
     },
   });
+
   return response.data;
 }
 
 export async function createNote(note: NoteCreate): Promise<Note> {
   const response = await axios.post<Note>(BASE_URL, note, {
-  headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_NTHB_TOKEN}`,
-  }
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`,
+    },
   });
   return response.data;
 }
 
-export async function deleteNote(id:string): Promise<Note>{
+export async function deleteNote(id: string): Promise<Note> {
   const url = `${BASE_URL}/${id}`;
-
-  const response= await axios.delete<Note>(url, {
+  const response = await axios.delete<Note>(url, {
     headers: {
-      Authorization: `Bearer ${import.meta.env.VITE_NTHB_TOKEN}`,
+      Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`,
     },
   });
-
   return response.data;
 }
